f5a070c1cec84e6dd1009375dcec697227ff2656
daniel04soden <dsoden09@gmail.com>
2026-05-03 18:05:47 +0100

    final commit to github, new commits found at git.danielsoden.xyz repositories

--- flake.nix
+++ flake.nix
@@ -1,115 +1,124 @@
 {
-  description = "Daniel's Sys Flake";
+	description = "Daniel's Sys Flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+		nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
-  };
-
-  outputs = inputs@{ self, nix-darwin, nixpkgs,nix-homebrew }:
-  let
-    configuration = {config, pkgs, ... }: {
-    system.primaryUser = "danielsoden";
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-
-      nixpkgs.config.allowUnfree = true;
-	
-	homebrew = {
-		enable = true;
-		brews = [
-		"mas"
-		];
-		#casks = [
-		# "exampleEntry"
-		#];
-		masApps = {
-		"Todoist" = 585829637;
-		};
-		onActivation.cleanup = "zap";
-		onActivation.autoUpdate = false;
-		onActivation.upgrade = true;
 	};
 
-        fonts.packages = [
-        pkgs.nerd-fonts.jetbrains-mono
-        ];
-      environment.systemPackages =
-        [
-          pkgs.neovim
-	        pkgs.ghostty-bin
+	outputs = inputs@{ self, nix-darwin, nixpkgs,nix-homebrew }:
+		let
+		configuration = {config, pkgs, ... }: {
+			system.primaryUser = "danielsoden";
+
+# List packages installed in system profile. To search by name, run:
+# $ nix-env -qaP | grep wget
+
+			nixpkgs.config.allowUnfree = true;
+
+			homebrew = {
+				enable = true;
+				brews = [
+					"mas"
+				];
+				casks = [
+				 "balenaetcher"
+				 "soulseek"
+				];
+				masApps = {
+					"Todoist" = 585829637;
+					"Amphetamine" = 937984704;
+				};
+				onActivation.cleanup = "zap";
+				onActivation.autoUpdate = false;
+				onActivation.upgrade = true;
+			};
+
+			fonts.packages = [
+				pkgs.nerd-fonts.jetbrains-mono
+			];
+			environment.systemPackages =
+				[
+					pkgs.neovim
+					pkgs.wget
+					pkgs.ghostty-bin
 					pkgs.mkalias
-	        pkgs.zed-editor
+					pkgs.zed-editor
 					pkgs.obsidian
-	        pkgs.fzf
+					pkgs.fzf
 					pkgs.rectangle
-        ];
+					pkgs.vesktop
+					pkgs.tailscale
+					pkgs.cyberduck
+				];
 
-        system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-            name = "system-applications";
-            paths = config.environment.systemPackages;
-            pathsToLink = [ "/Applications" ];
-        };
-        in
-        pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-            '';
+			system.activationScripts.applications.text = let
+				env = pkgs.buildEnv {
+					name = "system-applications";
+					paths = config.environment.systemPackages;
+					pathsToLink = [ "/Applications" ];
+				};
+			in
+				pkgs.lib.mkForce ''
+# Set up applications.
+				echo "setting up /Applications..." >&2
+				rm -rf /Applications/Nix\ Apps
+				mkdir -p /Applications/Nix\ Apps
+				find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+				while read -r src; do
+					app_name=$(basename "$src")
+						echo "copying $src" >&2
+						${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+						done
+						'';
 
 
-	system.defaults = {
-		dock.autohide = true;
-		NSGlobalDomain.AppleInterfaceStyle = "Dark";
-		NSGlobalDomain.KeyRepeat = 2;
+			system.defaults = {
+				dock.autohide = true;
+				NSGlobalDomain.AppleInterfaceStyle = "Dark";
+				NSGlobalDomain.KeyRepeat = 2;
+				trackpad.TrackpadThreeFingerDrag = true;
 
+			};
+
+			services.tailscale.enable = true;
+
+# Necessary for using flakes on this system.
+			nix.settings.experimental-features = "nix-command flakes";
+
+# Enable alternative shell support in nix-darwin.
+# programs.fish.enable = true;
+
+# Set Git commit hash for darwin-version.
+			system.configurationRevision = self.rev or self.dirtyRev or null;
+
+# Used for backwards compatibility, please read the changelog before changing.
+# $ darwin-rebuild changelog
+			system.stateVersion = 6;
+
+# The platform the configuration will be used on.
+			nixpkgs.hostPlatform = "aarch64-darwin";
+		};
+	in
+	{
+# Build darwin flake using:
+# $ darwin-rebuild build --flake .#daniels
+		darwinConfigurations."daniels" = nix-darwin.lib.darwinSystem {
+			modules = [
+				configuration
+					nix-homebrew.darwinModules.nix-homebrew
+					{
+						nix-homebrew = {
+							enable = true;
+							enableRosetta = true;
+							user = "danielsoden";
+						};
+					}
+
+			];
+		};
 	};
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#daniels
-    darwinConfigurations."daniels" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            enableRosetta = true;
-            user = "danielsoden";
-          };
-        }
-
-      ];
-    };
-  };
 }
